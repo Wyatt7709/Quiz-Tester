2cfc07bb11ff9aba8dfbb3f4d6ff67837c2f767c
Wyatt7709 <wwoydziak.7704@gmail.com>
2024-12-08 23:33:44 -0600

    Cypress

---
+++ cypress/Quiz.cy.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Quiz from '../client/src/components/Quiz'
+describe('<Quiz />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Quiz />)
+  })
+})
